c22c8fbe19fe6256f1b893fd64de8056981dcf37
854 <jahae854@gmail.com>
2023-07-03 18:12:47 +0900

    edit 파일이름변경

--- src/main/java/com/mysite/sbb/Cms854Application.java
+++ src/main/java/com/mysite/sbb/Cms854Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SbbApplication {
+public class Cms854Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SbbApplication.class, args);
+		SpringApplication.run(Cms854Application.class, args);
 	}
 
 }
